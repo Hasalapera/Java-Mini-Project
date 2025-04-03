0abb87f307e62ef68ae4588e8f845b3b83a69771
Hasala Perera <167463768+Hasalapera@users.noreply.github.com>
2025-04-04 02:44:15 +0530

    main gui

---
+++ aftercreategui/src/ui/Login.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel panel1;
+    private JTextField userNameTextField;
+    private JPasswordField passwordPasswordField;
+    private JButton cancelButton;
+    private JButton logInButton;
+}
